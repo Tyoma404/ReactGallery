aef311ee14fdb794417530b1650f8ea54bad3c5d
cisco <artemshot7@gmail.com>
2020-09-30 18:13:09 +0800

    change handler

--- components/LoginForm/index.js
+++ components/LoginForm/index.js
@@ -1,24 +1,35 @@
 import styles from './style.css'; // технология CSS-modules - уникальные стили для каждого компонента, чтобы не было конфликтов названий в разных css-файлах
 import React from 'react';
 
+async function Login(login, password){
+ 
+  let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyARBkhuz8A8LZgPc2WrhMkkuZkQ-yvvqLQ'
+  
+  let response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({"email": login, "password": password, "returnSecureToken": true})   
+  });
+
+  let commits = await response.json();
+
+return commits.idToken
+}  
 
 class LoginForm extends React.Component {
 
 
     state = {
-        loginClasses : styles.login        // в состоянии приложения храним какие CSS классы у нашей формы
+        loginClasses : styles.login ,
+        user: null,
+        password: null,     // в состоянии приложения храним какие CSS классы у нашей формы
             }
         
     errorHandler = (e) => {
       
        let errorclass = e.target.dataset.errorclass            
        
-       this.setState(prevState =>{         //  так меняется состояние, обязательно делать копию с помощью REST оператора ...    
-            return{
-                 ...prevState,
-                 loginClasses: styles.login + " " + errorclass          
-            }
-         })
+       this.setState({ loginClasses: styles.login + " " + errorclass}
+         )
 
          setTimeout(() => {
             
@@ -33,8 +44,26 @@ class LoginForm extends React.Component {
 
         }
 
+    changeHandler = (event) => {
+      console.log(event.target.value)
+      let temp = event.target.value
+      let name = event.target.name
+      this.setState({[name]: temp}) 
+
+
+      console.log({[name]: temp})
+    }
+    submitHandler = (e) =>{
+
+    }
+  
+
+
+
  
-render () {
+
+ 
+render() {
   console.log("loginClasses при каждом рендере компонента LoginForm" + this.state.loginClasses);
     return (
     <>
@@ -44,9 +73,9 @@ render () {
       <h2>Application Name</h2>
       <h4>Login</h4>
     </header>
-    <form className={styles.loginForm } action="#" method="post">
-      <input type="text" className={styles.loginInput} placeholder="User" required autoFocus/>
-      <input type="password" className={styles.loginInput} placeholder="Password" required/>
+    <form className={styles.loginForm } onSubmit={this.submitHandler} action="#" method="post">
+      <input name="user" type="text" onChange={this.changeHandler} className={styles.loginInput} placeholder="User" required autoFocus/>
+      <input name="password" type="password" onChange={this.changeHandler} className={styles.loginInput} placeholder="Password" required/>
       <div className="submitContainer">
         <button type="submit" className={styles.loginButton}>SIGN IN</button>
       </div>
@@ -60,8 +89,8 @@ render () {
     </>
 )
 }
-
 }
+
 
 
 
