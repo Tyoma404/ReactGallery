07006727f431228f28b98e30a4ecbe90c77e0926
tyoma404 <artemshot7@gmail.com>
2020-11-07 16:51:28 +0800

    blank progress bar

--- components/Blank/blank.js
+++ components/Blank/blank.js
@@ -16,58 +16,65 @@ const debounce = (func, delay) => {
 };
 
 export default function Test() {
-  const [progress, setProgress] = useState(70);
+  const [progress, setProgress] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
   const skipUpdate = null; // здесь в последнюю очередь будет функция-обертка для дебаунса и функции обновления состоянии прогресс-бара.
 
-  // картинка "https://fetch-progress.anthum.com/30kbps/images/sunrise-baseline.jpg"
+  let imageUrl =
+    "https://fetch-progress.anthum.com/30kbps/images/sunrise-baseline.jpg";
 
-  const downloadFile = url => {
+  const downloadFile = async url => {
     // Симулируем загрузку картинки чтобы вмешаться в процесс и показывать прогресс загрузки.
     // Сделаем запрос по адресу картинки, загрузим ответ как бинарный объект blob (Binary Large OBject).
     //
     // Через fetch слишком низкоуровневый код, используем популярную библиотеку axios (функция, как fetch). Ей для запроса передаем url
     // и объект с такими настройками (конкретные свойств взяты из документации axios, или гугл):
 
-    // responseType: "blob",
-    // onDownloadProgress: progressEvent => {
-    //   let percentCompleted = Math.floor(
-    //     (progressEvent.loaded / progressEvent.total) * 100
-    //
-    //           ...   обновляем прогресс бар, через состояние.
-    //
-    //   );
-
+    let response = await axios(url, {
+      responseType: "blob",
+      onDownloadProgress: progressEvent => {
+        let percentCompleted = Math.floor(
+          (progressEvent.loaded / progressEvent.total) * 100
+        );
+        setProgress(percentCompleted);
+        console.log("loaded:" + percentCompleted);
+      }
+    });
+    console.log(response.data);
     // отловить (catch) ошибки загрузки в консоль.
 
     const reader = new window.FileReader();
+
     reader.readAsDataURL(response.data); // к ответу (response - твоя переменная из запроса) цепляем специальный объект FileReader.
     reader.onload = () => {
       // FileReader используют еще когда делают Upload файлов на сайт.
-      document.getElementById("imgFetched").src = reader.result;
+      document.querySelector("#imgFetched").src = reader.result;
+      setLoaded(true);
     };
     ///  - загрузку сначала сделаем через кнопку Reload. Она напрямую запускает downloadFile(url)
     ///  - вторым этапом (когда уже все работает) настроим логику состояния, использовать хук useEffect чтобы в начале кнопки не было, downloadFile(url)
     ///  запускался при первом рендере этого компонента (Blank)
   };
-
+  console.log("test");
   const reset = () => {
     setProgress(0);
+    setLoaded(false);
+    document.querySelector("#imgFetched").src = "";
+    downloadFile(imageUrl);
   };
 
-  const reloadBtn = <button onClick={reset}>Reload</button>;
+  const reloadBtn = loaded ? <button onClick={reset}>Reload</button> : null;
+
+  useEffect(reset, []);
 
   return (
     <div id={styles.MyContainer}>
-      <h1> test container component</h1>
+      <h1> Show image component</h1>
 
       <MyBar now={progress} variant="determinate" color="primary" />
       <br />
-      <img
-        id="imgFetched"
-        width="900px"
-        src="https://fetch-progress.anthum.com/30kbps/images/sunrise-baseline.jpg"
-      />
+      <img id="imgFetched" width="900px" src="" />
       <br />
       {reloadBtn}
     </div>
